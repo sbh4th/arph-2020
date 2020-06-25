ee15f53a542bc5e923387388cbf5d29d4ac70716
Sam Harper <sbh4th@gmail.com>
2020-06-25 12:13:33 -0400

    Updated SuppFig8

--- code/rural-aadr-trends.R
+++ code/rural-aadr-trends.R
@@ -3,12 +3,11 @@
 #  input:    seer-ur-2013.txt
 #  output:   rural-aadr-trends.png
 #  project:  ARPH Life Expectancy
-#  author:   sam harper \ 2020-06-22
+#  author:   sam harper \ 2020-06-25
 
 ##### 0  #####
 ##### load libraries
 library(tidyverse)
-library(plotly)
 library(here)
 library(ggrepel)
 library(patchwork)
@@ -19,11 +18,11 @@ here::here()
 ##### 1  #####
 ##### Read in data
 
-# 2013 rural definitions
-d <- read_tsv(here("data", "seer-ur-2013.txt"), 
+# 2013 rural definitions from NCHS
+d <- read_tsv(here("data/seer-stat", "seer-ur-2013.txt"), 
                      col_names=c("ur","year","gender","aadr","deaths","pop"))
 
-# urban-rural definition
+# urban-rural definitions
 d$ur <- factor(d$ur, levels = c(0,1,2,3,4,5,6,7,8,9,10), 
   labels = c("Metro", "Metro >1m", "Metro >250k-1m", 
   "Metro <250k", "Non-metro", "Large urban, metro-adjacent", 
@@ -48,8 +47,7 @@ dur <- subset(d, (ur != "Metro" & ur != "Non-metro") & gender==0 & yearc>=1970)
 stheme <- theme_classic() + theme(plot.title = element_text(size = 18, face = "bold"), plot.subtitle = element_text(size=16)) + theme(axis.text.x = element_text(size = 16, colour = "grey60"), axis.title.y=element_text(size=16, angle=90, colour="grey60"), axis.text.y = element_text(size = 16, colour="grey60"), legend.position="none", panel.grid.major.y = element_line(linetype="dotted", colour="grey60"), panel.grid.major.x = element_line(colour="white"), panel.grid.minor = element_line(colour="white")) + theme(axis.line.x=element_line(colour="white"), axis.line.y=element_line(colour="white"), axis.ticks = element_blank(), strip.text = element_text(size = 16), strip.background = element_rect(colour="white"))
 
                   
-b <- 
-ggplot(dur, aes(x=yearc, y=aadr, colour=ur, label=ur)) + 
+b <- ggplot(dur, aes(x=yearc, y=aadr, colour=ur, label=ur)) + 
   geom_segment(aes(x = 1970, y = 600, xend = 2015, yend = 600), 
                linetype="dotted", color='grey60', show.legend=FALSE) +
   geom_segment(aes(x = 1970, y = 800, xend = 2015, yend = 800), 
